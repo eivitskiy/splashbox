2d51fcf35ab85b8bd08ebd543b4aa4bd0bfbe0f0
eivitskiy <eivitskiy@gmail.com>
2019-08-10 17:12:11 +0700

    Task 1 remaking

--- Task1.php
+++ Task1.php
@@ -1,19 +1,49 @@
 <?php
 
-function printBooleanArray($array)
+/**
+ * @param array $array
+ * @param int $lines
+ */
+function printBooleanArray(array $array, int $lines)
 {
-    foreach($array as $row) {
-        foreach($row as $column) {
-            echo (int)$column, "\t";
-        }
-        echo PHP_EOL;
+    $columns = intval(count($array) / $lines);
+
+    if(count($array) % $lines < ($lines - 1)) {
+        /*
+         * здесь нужно поместить обработчик ошибки, т.к.
+         * мы столкнулись с ситуацией когда строк в массиве слишком много
+         * т.е. если добавить ещё одну колонку - мы получим пустые строки,
+         * если же не добавлять - получаем недостаточное количество строк
+         */
+        exit('слишком много строк' . PHP_EOL);
     }
+
+    if(count($array) % $lines) {
+        $columns++;
+    }
+
+    foreach($array as $k => $v) {
+        if($k > 0 && $k % $columns == 0) {
+            echo PHP_EOL;
+        }
+        echo (int)$v, "\t";
+    }
+
+    echo PHP_EOL;
 }
 
 $arr = [
-    [true, false, false, true, false],
-    [false, true, false, true, false],
-    [false, false, true, true, false],
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    false
 ];
 
-printBooleanArray($arr);
+printBooleanArray($arr, 4);
